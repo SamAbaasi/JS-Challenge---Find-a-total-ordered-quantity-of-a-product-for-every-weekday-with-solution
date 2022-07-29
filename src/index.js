56712905dcb2803d03d56188961dd23838fbd807
Saman Abbasi <samyar1992@gmail.com>
2022-07-29 13:44:52 +0430

    add solution to solution.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ export default class OrdersAnalyzer {
     totalQuantity(productId, orders) {
         // TODO: Implement
         const result = {}
-        let productPerDay = this.weekdays.map(day => 0)
-        orders.forEach(order => {
-            const orderedDay = new Date(order.creationDate).getDay()
-            const productInLine = order.orderLines.find(product => product.productId === productId)
-            if(productInLine && productInLine.quantity) {
-                const prevQuantity = productPerDay[orderedDay]
-                productPerDay[orderedDay] = prevQuantity + productInLine.quantity
-            }
-        })
-        Object.keys(productPerDay).forEach(day => {
-            result[this.weekdays[parseInt(day)]] = productPerDay[parseInt(day)]
-        })
         return result
     }
 }
